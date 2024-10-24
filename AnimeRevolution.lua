757084a565b06fa19d17f34c235e42834eec875e
NOOB ZERO <121302058+vbbdv3@users.noreply.github.com>
2024-10-24 17:42:32 +0500

    Update and rename README.md to AnimeRevolution.lua

---
+++ AnimeRevolution.lua
@@ -0,0 +1,15 @@
+
+--Hueco Mundo
+_G.Defense_join_enabled = not _G.Defense_join_enabled
+while _G.Defense_join_enabled do
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(832.7273559570312, 29.913606643676758, 986.9053344726562)
+wait(0.75)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(818.8723754882812, 29.913606643676758, 923.8731079101562)
+wait(0.75)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(931.6915283203125, 29.913606643676758, 935.1910400390625)
+wait(0.75)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(980.467041015625, 29.913606643676758, 855.0782470703125)
+wait(0.75)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1092.0068359375, 29.913606643676758, 844.5345458984375)
+wait(0.75)
+end
