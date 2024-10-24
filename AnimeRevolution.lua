08561e7849ea29137f41bfd5db72056d31e4c0e1
NOOB ZERO <121302058+vbbdv3@users.noreply.github.com>
2024-10-24 19:42:23 +0500

    Update AnimeRevolution.lua

--- AnimeRevolution.lua
+++ AnimeRevolution.lua
@@ -13,3 +13,19 @@ wait(0.75)
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1092.0068359375, 29.913606643676758, 844.5345458984375)
 wait(0.75)
 end
+
+
+--Naruto
+_G.Defense_join_enabled = not _G.Defense_join_enabled
+while _G.Defense_join_enabled do
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1015.9475708007812, 13.472051620483398, -760.1246948242188)
+wait(0.75)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1055.374267578125, 13.472052574157715, -708.6597290039062)
+wait(0.75)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1077.2745361328125, 13.472051620483398, -666.218017578125)
+wait(0.75)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(955.7026977539062, 13.472051620483398, -569.5189208984375)
+wait(0.75)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(919.2747802734375, 15.040572166442871, -614.5482788085938)
+wait(0.75)
+end
